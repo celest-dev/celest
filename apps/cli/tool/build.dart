83da6902de0897fc87c66353e499756857b0e981
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2023-12-28 16:53:36 -0700

    ci: Create Windows appx package (#161)
    - On releases of the Celest CLI, create an appx package for distribution on Windows.
    - Adds preliminary infrastructure for EV code signing (w/ HSM)

--- apps/cli/tool/build.dart
+++ apps/cli/tool/build.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
 
 import 'package:archive/archive_io.dart';
+import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 import 'package:pubspec_parse/pubspec_parse.dart';
 
@@ -19,6 +21,9 @@ const entitlements = '''
 </dict>
 </plist>
 ''';
+
+final tempDir = Directory.systemTemp.createTempSync('celest_build_');
+final httpClient = http.Client();
 
 /// Builds and bundles the CLI for the current platform.
 ///
@@ -41,11 +46,12 @@ Future<void> main() async {
 
   print('Bundling CLI version $version for $osArch...');
 
+  final ext = Platform.isWindows ? 'appx' : 'zip';
   final filename = p.canonicalize(
     p.join(
       Platform.script.toFilePath(),
       '..',
-      'celest-$version-$osArch.zip',
+      'celest-$version-$osArch.$ext',
     ),
   );
   final buildDir = Directory.fromUri(Platform.script.resolve('../celest'));
@@ -55,8 +61,19 @@ Future<void> main() async {
 
   if (Platform.isMacOS) {
     await _codesignAndNotarize(buildDir);
+    await _createZip(fromDir: buildDir, outputPath: filename);
+  } else if (Platform.isWindows) {
+    await _createAppx(
+      buildDir: buildDir,
+      version: version,
+      outputPath: filename,
+    );
+    // await _evCodesign(filename);
+  } else if (Platform.isLinux) {
+    await _createZip(fromDir: buildDir, outputPath: filename);
+  } else {
+    throw UnsupportedError('Unsupported platform: ${Platform.operatingSystem}');
   }
-  await _createZip(fromDir: buildDir, outputPath: filename);
 
   print('Successfully wrote $filename');
 
@@ -88,7 +105,6 @@ Future<void> main() async {
 /// - https://scriptingosx.com/2021/07/notarize-a-command-line-tool-with-notarytool/
 /// - https://developer.apple.com/documentation/security/notarizing_macos_software_before_distribution/customizing_the_notarization_workflow
 Future<void> _codesignAndNotarize(Directory buildDir) async {
-  final tempDir = await Directory.systemTemp.createTemp('celest_build_');
   final entitlementsPlist = File(p.join(tempDir.path, 'entitlements.plist'));
   await entitlementsPlist.writeAsString(entitlements);
 
@@ -140,6 +156,217 @@ Future<void> _codesignAndNotarize(Directory buildDir) async {
   // xcrun notarytool submit --apple-id=$APPLE_ID --password=$APPLE_ID_PASSWORD --team-id=$TEAM_ID --wait --verbose celest.zip
   // xcrun stapler staple celest
   // spctl --assess --type exec -vv celest
+}
+
+// https://learn.microsoft.com/en-us/windows/win32/appxpkg/how-to-create-a-package-signing-certificate#step-2-create-a-private-key-using-makecertexe
+Future<void> _testCodesign(Directory buildDir) async {
+  await _runProcess(
+    'MakeCert',
+    [
+      '/n',
+      'CN=Celest, O=Teo, Inc., C=US',
+      '/r',
+      '/h',
+      '0',
+      '/eku',
+      '1.3.6.1.5.5.7.3.3,1.3.6.1.4.1.311.10.3.13',
+      '/e',
+      '01/01/2025',
+      '/sv',
+      'celest.pvk',
+      'celest.cer',
+    ],
+  );
+
+  await _runProcess(
+    'Pvk2Pfx',
+    [
+      '/pvk',
+      'celest.pvk',
+      '/spc',
+      'celest.cer',
+      '/pfx',
+      'celest.pfx',
+      '/po',
+      'password',
+    ],
+  );
+}
+
+final String _windowsSdkBinDir = () {
+  const binDir = r'C:\Program Files (x86)\Windows Kits\10\bin';
+  final allSdks = Directory(binDir)
+      .listSync()
+      .whereType<Directory>()
+      .map((e) => p.basename(e.path))
+      .where((path) => path.startsWith('10.'))
+      .toList();
+  allSdks.sort();
+  final latestSdk = allSdks.last;
+  return switch (Abi.current()) {
+    Abi.windowsArm64 => p.join(binDir, latestSdk, 'arm64'),
+    Abi.windowsX64 => p.join(binDir, latestSdk, 'x64'),
+    final unsupported =>
+      throw UnsupportedError('Unsupported ABI: $unsupported'),
+  };
+}();
+
+Future<void> _evCodesign(String appxPackage) async {
+  final evKeyRing = Platform.environment['EV_KEY_RING'];
+  final evKeyName = Platform.environment['EV_KEY_NAME'];
+
+  if (evKeyRing.isNullOrEmpty || evKeyName.isNullOrEmpty) {
+    throw StateError('EV_KEY_RING or EV_KEY_NAME is empty');
+  }
+  if (!evKeyRing!.startsWith('projects/')) {
+    throw StateError(
+      r'EV_KEY_RING must be of the form "projects/$projectId/locations/$location/keyRings/$keyRingName"',
+    );
+  }
+
+  print('Downloading GCP KMS integration...');
+  const gcpKmsUrl =
+      'https://github.com/GoogleCloudPlatform/kms-integrations/releases/download/v1.1/libkmsp11-1.1-windows-amd64.zip';
+
+  final gcpKmsZip = await httpClient.get(Uri.parse(gcpKmsUrl));
+  final decoder = ZipDecoder().decodeBytes(gcpKmsZip.bodyBytes);
+  extractArchiveToDisk(decoder, tempDir.path);
+
+  final pkcs11Config = File(p.join(tempDir.path, 'pkcs11.yaml'))..createSync();
+  pkcs11Config.writeAsStringSync(
+    '''
+tokens:
+  - key_ring: $evKeyRing
+ ''',
+    flush: true,
+  );
+
+  final opensslArgs = [
+    'req',
+    '-new',
+    '-subj',
+    '/CN=Teo Inc./O=Teo Inc./C=US',
+    '-sha256',
+    '-engine',
+    'pkcs11',
+    '-keyform',
+    'engine',
+    '-key',
+    'pkcs11:object=$evKeyName',
+  ];
+  final csr = Process.runSync(
+    'openssl',
+    opensslArgs,
+    environment: {
+      'PKCS11_MODULE_PATH': tempDir.path,
+      'KMS_PKCS11_CONFIG': pkcs11Config.path,
+    },
+    stdoutEncoding: utf8,
+    stderrEncoding: utf8,
+  );
+  if (csr.exitCode != 0) {
+    throw ProcessException(
+      'openssl',
+      opensslArgs,
+      csr.stderr.toString(),
+      csr.exitCode,
+    );
+  }
+}
+
+/// Windows-only. Creates an AppX installer package for the CLI.
+Future<void> _createAppx({
+  required Directory buildDir,
+  required String outputPath,
+  required String version,
+}) async {
+  // Mostly copied from winget: https://github.com/microsoft/winget-cli/blob/master/src/AppInstallerCLIPackage/Package.appxmanifest
+  // Schema: https://learn.microsoft.com/en-us/uwp/schemas/appxpackage/uapmanifestschema/element-application
+  final appxManifest = '''
+<?xml version="1.0" encoding="utf-8"?>
+<Package xmlns="http://schemas.microsoft.com/appx/manifest/foundation/windows10"
+         xmlns:uap="http://schemas.microsoft.com/appx/manifest/uap/windows10"
+         xmlns:uap3="http://schemas.microsoft.com/appx/manifest/uap/windows10/3"
+         xmlns:uap5="http://schemas.microsoft.com/appx/manifest/uap/windows10/5"
+         xmlns:rescap="http://schemas.microsoft.com/appx/manifest/foundation/windows10/restrictedcapabilities"
+         xmlns:com="http://schemas.microsoft.com/appx/manifest/com/windows10"
+         xmlns:desktop6="http://schemas.microsoft.com/appx/manifest/desktop/windows10/6"
+         IgnorableNamespaces="uap uap3 uap5 rescap">
+  <Identity Name="Celest.CLI"
+            Version="$version.0"
+            Publisher="CN=Teo Inc., O=Teo Inc., C=US" />
+  <Properties>
+    <DisplayName>Celest</DisplayName>
+    <PublisherDisplayName>Celest</PublisherDisplayName>
+    <Description>The CLI for Celest, the Flutter cloud platform.</Description>
+    <Logo>logo-full.png</Logo>
+    <desktop6:FileSystemWriteVirtualization>disabled</desktop6:FileSystemWriteVirtualization>
+    <desktop6:RegistryWriteVirtualization>disabled</desktop6:RegistryWriteVirtualization>
+  </Properties>
+  <Resources>
+    <Resource Language="en-us" />
+  </Resources>
+  <Dependencies>
+    <!-- Minimum supported version is 1809 (October 2018 Update, aka RS5) -->
+    <TargetDeviceFamily Name="Windows.Desktop" MinVersion="10.0.17763.0" MaxVersionTested="10.0.19033.0" />
+  </Dependencies>
+  <Applications>
+    <Application Id="celest" Executable="celest.exe" EntryPoint="Windows.FullTrustApplication" >
+      <uap:VisualElements DisplayName="NoUIEntryPoints"
+                          Description="The CLI for Celest, the Flutter cloud platform." 
+                          Square150x150Logo="logo-150x150.png" 
+                          Square44x44Logo="logo-44x44.png" 
+                          BackgroundColor="#FFFFFF"
+                          AppListEntry="none">
+        <uap:DefaultTile/>
+      </uap:VisualElements>
+      <Extensions>
+      <uap5:Extension Category="windows.appExecutionAlias">
+        <uap5:AppExecutionAlias>
+          <uap5:ExecutionAlias Alias="celest.exe" />
+        </uap5:AppExecutionAlias>
+      </uap5:Extension>
+    </Extensions>
+    </Application>
+  </Applications>
+  <Capabilities>
+    <rescap:Capability Name="runFullTrust" />
+    <rescap:Capability Name="unvirtualizedResources" />
+
+    <!-- See https://learn.microsoft.com/en-us/previous-versions/windows/apps/hh464936(v=win.10) -->
+    <Capability Name="internetClient" />
+    <Capability Name="internetClientServer" />
+    <Capability Name="privateNetworkClientServer" />
+  </Capabilities>
+</Package>
+''';
+
+  File(p.join(buildDir.path, 'AppxManifest.xml'))
+    ..createSync()
+    ..writeAsStringSync(appxManifest, flush: true);
+
+  // Copy logo files to buildDir.
+  // TODO: Update logos to be cleaner.
+  final sourceDir = p.dirname(p.fromUri(Platform.script));
+  for (final logo in [
+    'logo-full.png',
+    'logo-150x150.png',
+    'logo-44x44.png',
+  ]) {
+    File(p.join(sourceDir, logo)).copySync(p.join(buildDir.path, logo));
+  }
+
+  await _runProcess(
+    p.join(_windowsSdkBinDir, 'makeappx.exe'),
+    [
+      'pack',
+      '/d',
+      buildDir.path,
+      '/p',
+      outputPath,
+      '/v',
+    ],
+  );
 }
 
 Future<void> _runProcess(
