eb271ad9c2f708eda3c22fce344fc2d4d0bb3ded
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2024-01-22 19:54:31 -0800

    chore(cli): Throw when using old Dart SDK (#273)
    Throw early to not waste users' time when they're on an old version of Dart.
    
    https://celest.sentry.io/issues/4900661400

--- apps/cli/lib/init/pub/pub_environment.dart
+++ apps/cli/lib/init/pub/pub_environment.dart
@@ -1,8 +1,9 @@
+import 'package:celest_cli_common/celest_cli_common.dart';
 import 'package:pub_semver/pub_semver.dart';
 
 abstract final class PubEnvironment {
   /// The version of the Dart SDK that this version of the CLI is compatible
   /// with.
   static final VersionConstraint dartSdkConstraint =
-      VersionConstraint.compatibleWith(Version.parse('3.2.0'));
+      VersionConstraint.compatibleWith(minSupportedDartSdk);
 }
