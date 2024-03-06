c04a5d936be9a1dbb1b7499085a9917dab09bf56
Dillon Nys <dillon.andre.nys@gmail.com>
2024-03-06 15:21:37 -0800

    feat(core): Remaining secure storage bindings

--- packages/celest_core/tool/ffigen.sh
+++ packages/celest_core/tool/ffigen.sh
@@ -8,9 +8,18 @@ flutter pub get
 flutter build apk
 popd
 
+echo "Checking out submodules..."
+git submodule update --init
+
 echo "Generating FFI bindings..."
 dart run ffigen --config=ffigen.core_foundation.yaml
 dart run ffigen --config=ffigen.security.yaml
+if "$(uname)" == "Darwin"; then 
+    echo "Skipping Linux bindings." >&2
+else 
+    dart run ffigen --config=ffigen.glib.yaml
+    dart run ffigen --config=ffigen.libsecret.yaml
+fi
 
 echo "Generating JNI bindings..."
 dart run jnigen --config=jnigen.yaml
