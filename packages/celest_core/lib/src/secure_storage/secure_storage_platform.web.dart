f85989b87450f2abd7bf23eb2d2a2007a04b9483
Dillon Nys <dillon.andre.nys@gmail.com>
2024-03-06 22:33:02 -0800

    add web impl, separate memory impl

--- packages/celest_core/lib/src/secure_storage/secure_storage_platform.web.dart
+++ packages/celest_core/lib/src/secure_storage/secure_storage_platform.web.dart
@@ -1,15 +1,23 @@
 import 'package:celest_core/src/secure_storage/secure_storage.dart';
-import 'package:celest_core/src/secure_storage/secure_storage.stub.dart';
-import 'package:meta/meta.dart';
 
-abstract base class SecureStoragePlatform implements SecureStorage {
-  factory SecureStoragePlatform({
+final class SecureStoragePlatform implements SecureStorage {
+  SecureStoragePlatform({
     String? scope,
-  }) =>
-      SecureStoragePlatformStub(scope: scope ?? _defaultScope);
+  }) : _impl = MemorySecureStorage(scope: scope ?? _defaultScope);
+
+  final SecureStorage _impl;
 
   static const _defaultScope = 'dev.celest.celest';
 
-  @protected
-  const SecureStoragePlatform.base();
+  @override
+  void clear() => _impl.clear();
+
+  @override
+  String? delete(String key) => _impl.delete(key);
+
+  @override
+  String? read(String key) => _impl.read(key);
+
+  @override
+  String write(String key, String value) => _impl.write(key, value);
 }
