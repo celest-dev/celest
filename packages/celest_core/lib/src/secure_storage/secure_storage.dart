eeae84b3ff50dd626c973462d1b8c81725f3033f
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2024-03-06 22:51:37 -0800

    chore(core): Secure storage clean up (#61)
    - Separate memory implementation into its own class
    - Clean up Linux implementation

--- packages/celest_core/lib/src/secure_storage/secure_storage.dart
+++ packages/celest_core/lib/src/secure_storage/secure_storage.dart
@@ -9,3 +9,25 @@ abstract interface class SecureStorage {
   String? delete(String key);
   void clear();
 }
+
+/// An in-memory implementation of [SecureStorage].
+final class MemorySecureStorage implements SecureStorage {
+  MemorySecureStorage({
+    required this.scope,
+  });
+
+  final _storage = <String, String>{};
+  final String scope;
+
+  @override
+  void clear() => _storage.removeWhere((key, _) => key.startsWith('$scope/'));
+
+  @override
+  String? delete(String key) => _storage.remove('$scope/$key');
+
+  @override
+  String? read(String key) => _storage['$scope/$key'];
+
+  @override
+  String write(String key, String value) => _storage['$scope/$key'] = value;
+}
--- packages/celest_core/lib/src/secure_storage/secure_storage_platform.web.dart
+++ packages/celest_core/lib/src/secure_storage/secure_storage_platform.web.dart
@@ -1,15 +1,9 @@
 import 'package:celest_core/src/secure_storage/secure_storage.dart';
-import 'package:celest_core/src/secure_storage/secure_storage.stub.dart';
-import 'package:meta/meta.dart';
 
-abstract base class SecureStoragePlatform implements SecureStorage {
-  factory SecureStoragePlatform({
-    String? scope,
-  }) =>
-      SecureStoragePlatformStub(scope: scope ?? _defaultScope);
+extension type SecureStoragePlatform._(SecureStorage _impl)
+    implements SecureStorage {
+  SecureStoragePlatform({String? scope})
+      : _impl = MemorySecureStorage(scope: scope ?? _defaultScope);
 
   static const _defaultScope = 'dev.celest.celest';
-
-  @protected
-  const SecureStoragePlatform.base();
 }
