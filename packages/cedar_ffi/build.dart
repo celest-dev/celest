80529b432c52441dd06f8187443c064ac73c0864
Dillon Nys <dillon.andre.nys@gmail.com>
2024-03-25 16:15:35 -0700

    Update build script

--- packages/cedar_ffi/build.dart
+++ packages/cedar_ffi/build.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:native_assets_cli/native_assets_cli.dart';
 
 const packageName = 'cedar_ffi';
-const ffiPackageName = 'cedar_ffi';
 
 final IOSink buildLogs = () {
   final logsFile = File.fromUri(
@@ -14,52 +13,47 @@ final IOSink buildLogs = () {
   return logsFile.openWrite(mode: FileMode.write);
 }();
 
-/// Implements the protocol from `package:native_assets_cli` by building
-/// the C code in `src/` and reporting what native assets it built.
 void main(List<String> args) async {
-  // Parse the build configuration passed to this CLI from Dart or Flutter.
-  final buildConfig = await BuildConfig.fromArgs(args);
-  buildLogs.writeln(buildConfig.toString());
-  final buildOutput = BuildOutput()
-    ..dependencies.dependencies.addAll([
-      buildConfig.packageRoot.resolve('build.dart'),
-      buildConfig.packageRoot.resolve('src/'),
-    ]);
+  try {
+    await build(args, (config, output) async {
+      buildLogs.writeln(config.toString());
 
-  // Build the Rust code in `src/` to `target/`.
-  //
-  // Since we're in a workspace, this will default to the repo root which we
-  // don't want.
-  final cargoOutput = buildConfig.packageRoot.resolve('target/');
-  await runProcess(
-    'cargo',
-    ['build', '--release'],
-    environment: {
-      'CARGO_TARGET_DIR': cargoOutput.toFilePath(),
-    },
-    workingDirectory: buildConfig.packageRoot.resolve('src').toFilePath(),
-  );
+      output.addDependency(config.packageRoot.resolve('src/'));
 
-  final binaryName = buildConfig.targetOs.dylibFileName(ffiPackageName);
-  final binaryOut = cargoOutput.resolve('release/$binaryName');
-  if (!File.fromUri(binaryOut).existsSync()) {
-    throw Exception('$binaryOut does not exist');
+      // Build the Rust code in `src/` to `target/`.
+      //
+      // Since we're in a workspace, this will default to the repo root which we
+      // don't want.
+      final cargoOutput = config.packageRoot.resolve('target/');
+      await runProcess(
+        'cargo',
+        ['build', '--release'],
+        environment: {
+          'CARGO_TARGET_DIR': cargoOutput.toFilePath(),
+        },
+        workingDirectory: config.packageRoot.resolve('src').toFilePath(),
+      );
+
+      final binaryName = config.targetOS.dylibFileName(packageName);
+      final binaryOut = cargoOutput.resolve('release/$binaryName');
+      if (!File.fromUri(binaryOut).existsSync()) {
+        throw Exception('$binaryOut does not exist');
+      }
+      final nativeAsset = NativeCodeAsset(
+        package: packageName,
+        name: 'src/ffi/cedar_bindings.g.dart',
+        linkMode: DynamicLoadingBundled(),
+        os: config.targetOS,
+        architecture: config.targetArchitecture,
+        file: binaryOut,
+      );
+      buildLogs.writeln('Compiled asset: ${nativeAsset.toString()}');
+      output.addAsset(nativeAsset);
+    });
+  } finally {
+    await buildLogs.flush();
+    await buildLogs.close();
   }
-  final nativeAsset = Asset(
-    id: 'package:$packageName/src/ffi/cedar_bindings.g.dart',
-    linkMode: LinkMode.dynamic,
-    target: buildConfig.target,
-    path: AssetAbsolutePath(binaryOut),
-  );
-  buildLogs.writeln('Compiled asset: ${nativeAsset.toString()}');
-  buildOutput.assets.add(nativeAsset);
-
-  // Write the output according to the native assets protocol so that Dart or
-  // Flutter can find the native assets produced by this script.
-  await buildOutput.writeToFile(outDir: buildConfig.outDir);
-
-  await buildLogs.flush();
-  await buildLogs.close();
 }
 
 Future<void> runProcess(
