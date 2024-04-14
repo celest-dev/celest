8fb516a0f9b3b6e0da884b7e8625a2b630fecd15
Dillon Nys <dillon.andre.nys@gmail.com>
2024-04-14 09:36:28 -0700

    Remove Cedar packages

--- packages/cedar/lib/src/util.dart
+++
@@ -1,8 +0,0 @@
-extension Let<T extends Object> on T? {
-  R? let<R>(R Function(T) f) {
-    if (this case final this_?) {
-      return f(this_);
-    }
-    return null;
-  }
-}
--- packages/cedar_ffi/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## 0.1.1
-
-- Add template-linked policies
-
-## 0.1.0
-
-- Initial version.
