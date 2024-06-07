ab8278b35e32759d3ea083d4780759b41b5c9a02
Dillon Nys <dillon.andre.nys@gmail.com>
2024-06-07 11:47:22 -0700

    fix: Allow `null` for `@httpQuery` and `@httpHeader` parameters
    Default to the parameter name for unspecified httpQuery/httpHeader names.

--- packages/celest/lib/src/functions/http/http_query.dart
+++ packages/celest/lib/src/functions/http/http_query.dart
@@ -6,8 +6,10 @@ import 'package:meta/meta_meta.dart';
 @Target({TargetKind.parameter})
 final class httpQuery {
   /// {@macro celest.functions.http.http_query}
-  const httpQuery(this.name);
+  const httpQuery([this.name]);
 
   /// The name of the HTTP query parameter.
-  final String name;
+  ///
+  /// If `null`, the name of the parameter will be used.
+  final String? name;
 }
