cf1d09fb3540f1a17cf8246062e0dbc33b2bd184
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2024-10-04 17:46:26 -0700

    chore(auth): Add CLI support for SMS (#189)

--- packages/celest/lib/src/auth/auth_provider.dart
+++ packages/celest/lib/src/auth/auth_provider.dart
@@ -3,14 +3,23 @@
 ///
 /// Currently, Celest supports the following authentication methods:
 /// - [AuthProvider.email] Email sign-in with OTP codes.
+/// - [AuthProvider.sms] SMS sign-in with OTP codes.
 /// {@endtemplate}
 sealed class AuthProvider {
+  /// {@macro celest.auth.auth_provider}
   const AuthProvider();
 
   /// A provider which enables email sign-in with OTP codes.
   const factory AuthProvider.email() = _EmailAuthProvider;
+
+  /// A provider which enables SMS sign-in with OTP codes.
+  const factory AuthProvider.sms() = _SmsAuthProvider;
 }
 
 final class _EmailAuthProvider extends AuthProvider {
   const _EmailAuthProvider();
+}
+
+final class _SmsAuthProvider extends AuthProvider {
+  const _SmsAuthProvider();
 }
