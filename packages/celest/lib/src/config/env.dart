c74e324ddbbf5d785f968adf74d55afa59e2b83c
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2024-10-06 13:53:51 -0700

    feat: Add remaining auth providers and secrets (#190)
    - Adds support for remaining auth providers
    - Adds support for secrets

--- packages/celest/lib/src/config/env.dart
+++ packages/celest/lib/src/config/env.dart
@@ -1,4 +1,22 @@
-import 'package:celest/src/core/cloud_widget.dart';
+import 'package:celest/src/core/context.dart';
+
+/// A configuration value that can be used to configure a Celest backend.
+sealed class ConfigurationValue implements ContextKey<String> {
+  const ConfigurationValue._(this.name);
+
+  /// The name of the configuration value in the environment.
+  final String name;
+
+  @override
+  String? read(Context context) {
+    return context.platform.environment[name];
+  }
+
+  @override
+  void set(Context context, String? value) {
+    throw UnsupportedError('Cannot set a configuration value at runtime.');
+  }
+}
 
 /// {@template celest.config.environment_variable}
 /// A reference to an environment variable within a Celest backend.
@@ -8,12 +26,23 @@ import 'package:celest/src/core/cloud_widget.dart';
 /// configuration to your backend. They allow you to keep configuration values
 /// separate from your codebase, improving flexibility when running in different
 /// environments.
-final class EnvironmentVariable extends CloudWidget {
+final class env extends ConfigurationValue {
   /// {@macro celest.config.environment_variable}
-  const EnvironmentVariable({
-    required this.name,
-  });
+  const env(super.name) : super._();
 
-  /// The name of the environment variable as it appears in its environment.
-  final String name;
+  /// The active Celest environment.
+  ///
+  /// For example, `production`.
+  static const env environment = env('CELEST_ENVIRONMENT');
+}
+
+/// {@template celest.config.secret}
+/// A secret value that should be kept confidential within a Celest backend.
+///
+/// Secrets behave much like environment variables except that their values
+/// are encrypted in the transit and only exposed to the backend itself.
+/// {@endtemplate}
+final class secret extends ConfigurationValue {
+  /// {@macro celest.config.secret}
+  const secret(super.name) : super._();
 }
