aa808e82c71cc6556718e938d4eb7ed938e0d1ea
Dillon Nys <dillon@celest.dev>
2024-10-14 12:22:29 -0700

    chore: Add Supabase + Firebase examples
    Adds example apps showing how to integrate Supabase + Firebase auth as auth providers in a Celest project.

---
+++ examples/firebase/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import firebase_auth
+import firebase_core
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+}
