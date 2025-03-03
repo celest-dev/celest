9fc136e682f684e94976645ec94cf21d254dffe9
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2025-03-03 16:22:30 -0700

    chore(cli): Remove OpenAPI code (#1077)
    To be added back later, potentially. But right now, it is a half-broken mess.

--- apps/cli/lib/openapi/model/openapi_name_allocator.dart
+++
@@ -1,7 +0,0 @@
-class OpenApiNameAllocator {
-  // String allocate(OpenApiType type, [String? context]) {
-  //   if (type.primitiveType case final primitiveType?) {
-  //     name
-  //   }
-  // }
-}
--- apps/cli/lib/openapi/stripe/stripe_type_resolver.dart
+++
@@ -1,6 +0,0 @@
-import 'package:celest_cli/openapi/stripe/stripe_generator_context.dart';
-import 'package:celest_cli/openapi/type/openapi_type_schema_resolver.dart';
-
-final class StripeTypeResolver extends OpenApiTypeSchemaResolver {
-  StripeTypeResolver({required StripeOpenApiGeneratorContext super.context});
-}
