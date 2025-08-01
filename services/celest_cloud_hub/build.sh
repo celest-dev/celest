a36c73eac58f96740e4b899404ebe73478536399
Dillon Nys <24740863+dnys1@users.noreply.github.com>
2025-08-01 14:43:27 -0700

    chore(cloud_hub): Add dev environment (#445)

--- services/celest_cloud_hub/build.sh
+++ services/celest_cloud_hub/build.sh
@@ -3,19 +3,20 @@
 # This script builds the Celest Cloud Hub Docker image.
 #
 # Usage:
-#   ./build.sh [--push]
+#   ./build.sh [tag=latest] [--push]
 
 set -e
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
+TAG=${1:-latest}
 
 docker build \
   --platform linux/amd64,linux/arm64 \
-  -t cloud-hub:latest \
-  -t celestdev/cloud-hub:latest \
+  -t cloud-hub:$TAG \
+  -t celestdev/cloud-hub:$TAG \
   -f "$REPO_ROOT/services/celest_cloud_hub/Dockerfile" \
   "$REPO_ROOT"
 
-if [[ "$1" == "--push" ]]; then
-    docker push celestdev/cloud-hub:latest
+if [[ "$2" == "--push" ]]; then
+    docker push celestdev/cloud-hub:$TAG
 fi
